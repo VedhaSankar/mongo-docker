1ad9e8d4fa7aa0670b2d68e1f4e6212eae2c0050
VedhaSankar <vedhasankar26@gmail.com>
2021-08-19 00:07:18 +0530

    Updates for 2021-08-19 00:07:18

---
+++ app.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+import pymongo.errors as pymon_err
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.environ.get('MONGO_URI')
+
+# creating a MongoClient object  
+client = MongoClient(MONGO_URI)  
+
+# accessing the database  
+DB_NAME = 'trials'
+database = client[DB_NAME]
+
+
+
+def insert_records(data):
+
+    result = {
+
+        "result"    : "success"
+
+    }
+
+    collection_name = 'docker'
+    new_collection = database[collection_name]
+
+    x = new_collection.insert_one(result)
+    print(x)
+
+
+def startpy():
+
+    insert_records()
+
+
+if __name__ == '__main__':
+
+    startpy()
+
