27b3e0780a041162ab921bc7283c8f513cb6eb70
VedhaSankar <vedhasankar26@gmail.com>
2022-03-03 12:58:07 +0530

    Updates for 12:58:07   03/03/22

--- mongo_crud.py
+++ mongo_crud.py
@@ -1,3 +1,4 @@
+from socket import AI_PASSIVE
 import pymongo
 from pymongo import MongoClient
 # import pymongo.errors as pymon_err
@@ -29,9 +30,33 @@ def insert_records():
     print(x)
 
 
+# def read_records():
+#     print (DB_NAME.database.find())
+
+    # for record in database.find():
+    #     print (record)
+
+def add_unique_key_index(collection_name):
+    """
+        db.members.createIndex( { "user_id": 1 }, { "unique": True } )
+    """
+    # access collection of the database  
+    database[collection_name].create_index([("project_id", pymongo.ASCENDING)])
+
+
+def get_collections():
+
+    collections = database.list_collection_names()
+
+    for collect in collections: 
+        print(collect) 
+
 def startpy():
 
-    insert_records()
+    # insert_records()
+    # read_records()
+    add_unique_key_index('docker')
+
 
 
 if __name__ == '__main__':
