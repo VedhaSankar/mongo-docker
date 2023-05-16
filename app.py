e4cf2710bb3d1a7e0f8962d959ba4e860985286f
VedhaSankar <vedhasankar26@gmail.com>
2023-05-16 08:37:02 +0530

    added override for the env variables

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 
 MONGO_URI = os.environ.get('MONGO_URI')
 client = MongoClient(MONGO_URI)  
